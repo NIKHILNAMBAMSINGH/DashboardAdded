134e8e1207ad12eaff1c4734f59fbfdfeeaddbf4
Nikhil Nambam Singh <nambamnikhil8@gmail.com>
2024-05-13 16:35:35 +0530

    dropdownstyle added

--- js/bootstrap.bundle.js
+++ js/bootstrap.bundle.js
@@ -65,6 +65,8 @@
    * ------------------------------------------------------------------------
    */
 
+
+
   var TRANSITION_END = 'transitionend';
   var MAX_UID = 1000000;
   var MILLISECONDS_MULTIPLIER = 1000; // Shoutout AngusCroll (https://goo.gl/pxwQGp)
